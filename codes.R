910df61b77b9c405c5ce1a7679d862e4df9a07f1
CharlesLambert70 <56747304+CharlesLambert70@users.noreply.github.com>
2025-06-13 09:34:59 +0800

    Create codes.R

---
+++ codes.R
@@ -0,0 +1,8 @@
+This is the codes for two-sample Mendelian randomization analyses for the causal effects of T2D-related pathways, insulin secretion and insulin sensitivity on birth weight.
+
+library(TwoSampleMR)
+library(LDlinkR)
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(stringr)
