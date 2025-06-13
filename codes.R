b4f72d6351f35e377998e928f5f6d7ee3f37025d
CharlesLambert70 <56747304+CharlesLambert70@users.noreply.github.com>
2025-06-13 09:37:08 +0800

    Update codes.R

--- codes.R
+++ codes.R
@@ -1,4 +1,4 @@
-This is the codes for two-sample Mendelian randomization analyses for the causal effects of T2D-related pathways, insulin secretion and insulin sensitivity on birth weight.
+#This is the codes for two-sample Mendelian randomization analyses for the causal effects of T2D-related pathways, insulin secretion and insulin sensitivity on birth weight.
 
 library(TwoSampleMR)
 library(LDlinkR)
@@ -6,3 +6,9 @@ library(data.table)
 library(dplyr)
 library(tidyr)
 library(stringr)
+
+#load instrumental variables for exposures
+T2DGGI_SNPs = read.csv("/T2DGGI_SNPs.csv", header = TRUE)  
+Insulin_secretion_SNPs = read.csv("/Insulin_secretion_SNPs.csv", header = TRUE)  
+Insulin_sensitivity_SNPs = read.csv("/Insulin_sensitivity_SNPs.csv", header = TRUE)  
+
